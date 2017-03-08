ea893da2ec94956f8b81ee6d10df42099152ecf0
Jan Hellar <jhellar@redhat.com>
2017-03-08 19:09:46 +0100

    Added config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  host: '',
+  username: '',
+  password: '',
+  environment: '',
+  iosPlatform: 'iOS Simulator,name=iPhone 7',
+  iosUsername: '',
+  iosPushBundleId: '',
+  iosPushP12Password: '',
+  iosPushTeam: '',
+  iosPushAppIDName: '',
+  iosPushDevelopment: true
+};
